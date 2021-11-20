db4250dee0a2ab2775b5278ca3df635a05e31ef4
qew34uw6 <qew34uw6@outlook.com>
2021-11-20 09:35:18 -0800

    fix test names (#1327)

--- bulkhead/src/test/java/com/iluwatar/bulkhead/SemaphoreBulkheadTest.java
+++ bulkhead/src/test/java/com/iluwatar/bulkhead/SemaphoreBulkheadTest.java
@@ -65,7 +65,7 @@ public class SemaphoreBulkheadTest {
   }
 
   @Test
-  public void throwsExceptionWhenBulkheadOverFullAfterWaitingTime() throws InterruptedException {
+  public void throwsExceptionWhenBulkheadStillFullAfterWaitingTime() throws InterruptedException {
     final SemaphoreBulkhead bulkhead = new SemaphoreBulkhead(2, 500);
     final Runnable runnableWithBulkhead = bulkhead.decorate(runnable);
     final AtomicReference<Exception> exception = new AtomicReference<>();
@@ -93,7 +93,7 @@ public class SemaphoreBulkheadTest {
   }
 
   @Test
-  public void shouldSuccessWhenBulkheadAvaibleAgain() throws InterruptedException {
+  public void shouldSuccessWhenBulkheadAvailableAgain() throws InterruptedException {
     final SemaphoreBulkhead bulkhead = new SemaphoreBulkhead(1, 5000);
     final Runnable runnableWithBulkhead = bulkhead.decorate(runnable);
     final Thread thread1 = new Thread(runnableWithBulkhead);
